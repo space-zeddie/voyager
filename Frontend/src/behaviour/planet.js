0992a1b713412cf618d90b4bafb20a7e6f554b64
space-zeddie <thespacezeddie@gmail.com>
2016-06-07 16:58:24 +0300

    added an array for storing planets

--- Frontend/src/behaviour/planet.js
+++ Frontend/src/behaviour/planet.js
@@ -1,35 +1,40 @@
-var planet = {
-    $planet: null,
-    radiusField: 0,
-    radiusPlanet: 0,
-    centerX: 0,
-    centerY: 0
-};
-
-function overlapsField(x, y) {
-    var val = (x - planet.centerX) * (x - planet.centerX) + (y - planet.centerY) * (y - planet.centerY);
-    return val <= planet.radiusField * planet.radiusField;
-}
-
-function collides(x, y) {
-    var val = (x - planet.centerX) * (x - planet.centerX) + (y - planet.centerY) * (y - planet.centerY);
-    return val <= planet.radiusPlanet * planet.radiusPlanet;
-}
+var planets = [];
 
 function init($elem) {
-    planet.$planet = $elem;
-    var $gravity = planet.$planet.find('.gravity-field');
-    planet.radiusField = planet.$planet.find('.gravity-field').width() / 2;
-    planet.radiusPlanet = planet.$planet.find('.planet-surface').width() / 2;
-
-    planet.centerX = $gravity.offset().left + $gravity.width() / 2;
-    planet.centerY = $gravity.offset().top + $gravity.height() / 2;
+    var $gravity = $elem.find('.gravity-field');
     
-   /* $planet.click(function (e) {
+    var planet = {
+        $planet: $elem,
+        radiusField: $gravity.width() / 2,
+        radiusPlanet: $elem.find('.planet-surface').width() / 2,
+        centerX: $gravity.offset().left + $gravity.width() / 2,
+        centerY: $gravity.offset().top + $gravity.height() / 2,
+        collides: collides,
+        overlaps: overlapsField
+    };
+    
+    function overlapsField(x, y) {
+        var val = (x - planet.centerX) * (x - planet.centerX) + (y - planet.centerY) * (y - planet.centerY);
+        return val <= planet.radiusField * planet.radiusField;
+    }
+
+    function collides(x, y) {
+        var val = (x - planet.centerX) * (x - planet.centerX) + (y - planet.centerY) * (y - planet.centerY);
+        return val <= planet.radiusPlanet * planet.radiusPlanet;
+    }
+    
+    planet.$planet.click(function (e) {
         var x = e.pageX;
         var y = e.pageY;
         alert(collides(x, y));
-    });*/
+    });
+    
+    planets.push(planet);
+}
+
+function allPlanets() {
+    return planets;
 }
 
 exports.init = init;
+exports.allPlanets = allPlanets;
