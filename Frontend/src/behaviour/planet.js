e8019b643513e7c470a1d029e67a7f778d1bd7f3
space-zeddie <thespacezeddie@gmail.com>
2016-06-07 14:42:36 +0300

    added the function that checks whether a point is on the planet

--- Frontend/src/behaviour/planet.js
+++ Frontend/src/behaviour/planet.js
@@ -9,23 +9,25 @@ function overlapsField(x, y) {
     return val <= radiusField * radiusField;
 }
 
-
+function collides(x, y) {
+    var val = (x - centerX) * (x - centerX) + (y - centerY) * (y - centerY);
+    return val <= radiusPlanet * radiusPlanet;
+}
 
 function init($elem) {
     $planet = $elem;
     var $gravity = $planet.find('.gravity-field');
     radiusField = $planet.find('.gravity-field').width() / 2;
     radiusPlanet = $planet.find('.planet-surface').width() / 2;
-    //alert(radiusField + ', ' + radiusPlanet);
 
     centerX = $gravity.offset().left + $gravity.width() / 2;
     centerY = $gravity.offset().top + $gravity.height() / 2;
     
-    $planet.click(function (e) {
+   /* $planet.click(function (e) {
         var x = e.pageX;
         var y = e.pageY;
-        alert(overlapsField(x, y));
-    });
+        alert(collides(x, y));
+    });*/
 }
 
 exports.init = init;
