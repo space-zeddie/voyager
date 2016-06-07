349e2182d2a8a5f9397543df7d2b231a84c21a11
space-zeddie <thespacezeddie@gmail.com>
2016-06-07 15:51:27 +0300

    added rudimentary movement to the right

--- Frontend/src/behaviour/game.js
+++ Frontend/src/behaviour/game.js
@@ -5,6 +5,17 @@ var animFrame = null;
 
 function init(canvas, animFrame) {
     generator.generatePlanet();
+    
+    $(document).keydown(function (e) {
+        if (e.which === 39) {
+            var planets = $('.planets').find('.planet').css('left', '+=10px');
+           // alert(JSON.stringify($('.planets').children()));
+            /*planets.forEach(function (elem) {
+                alert('elem');
+                elem.position.left += 10;
+            });*/
+        }
+    });
 }
 
 exports.init = init;
