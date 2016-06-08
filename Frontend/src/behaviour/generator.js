94e5c3fb31caad07c23c0f88aa65f375b8c45ac3
space-zeddie <thespacezeddie@gmail.com>
2016-06-08 10:06:00 +0300

    wrote a method for instantiating a random planet div

--- Frontend/src/behaviour/generator.js
+++ Frontend/src/behaviour/generator.js
@@ -2,6 +2,7 @@ var planets = require('./planet');
 
 function generatePlanet() {
     planets.init($('.planet'));
+    planets.randomPlanet();
 }
 
 exports.generatePlanet = generatePlanet;
--- Frontend/src/behaviour/planet.js
+++ Frontend/src/behaviour/planet.js
@@ -1,17 +1,41 @@
 var planets = [];
+var OFFSET = 10;
+var fieldSizes = ['small', 'medium', 'large'];
+var planetSizes = ['regular', 'dwarf', 'giant'];
+var planetColours = ['red', 'blue', 'cornsilk'];
 
-function init($elem) {
-    var $gravity = $elem.find('.gravity-field');
+var PLANET_TEMPLATE = '<div class="planet"><div class="gravity-field"><div class="planet-surface"></div></div></div>';
+var $planets = $('.planets');
+
+function planetOverlap(planet1, planet2) {
+    var lim = planet1.radiusPlanet + planet2.radiusPlanet + Math.max(planet1.radiusField, planet2.radiusField) - OFFSET;
+    var dist = Math.sqrt( Math.pow(planet1.centerX - planet2.centerX, 2) + Math.pow(planet1.centerY - planet2.centerY, 2) );
+    return dist <= lim;
+}
+
+function randomPlanetDiv() {
+    var sizeOuter = Math.floor(Math.random() * 3);
+    var sizeInner = Math.floor(Math.random() * 3);
+    var colour = Math.floor(Math.random() * 3);
     
-    var planet = {
-        $planet: $elem,
-        radiusField: $gravity.width() / 2,
-        radiusPlanet: $elem.find('.planet-surface').width() / 2,
-        centerX: $gravity.offset().left + $gravity.width() / 2,
-        centerY: $gravity.offset().top + $gravity.height() / 2,
-        collides: collides,
-        overlaps: overlapsField
-    };
+    var fieldSize = fieldSizes[sizeOuter];
+    var planetSize = planetSizes[sizeInner];
+    var planetColour = planetColours[colour];
+    
+    var $elem = $(PLANET_TEMPLATE);
+    $elem.addClass(fieldSize);
+    $elem.find('.planet-surface').addClass(planetSize);
+    $elem.find('.planet-surface').addClass(planetColour);
+    
+    // TEST
+    $elem.css('left', '+=200px');
+    planets.push(createPlanet($elem));
+    alert($elem.html());
+    $planets.append($elem);
+}
+
+function createPlanet($elem) {
+    var $gravity = $elem.find('.gravity-field');
     
     function overlapsField(x, y) {
         var val = (x - planet.centerX) * (x - planet.centerX) + (y - planet.centerY) * (y - planet.centerY);
@@ -23,13 +47,21 @@ function init($elem) {
         return val <= planet.radiusPlanet * planet.radiusPlanet;
     }
     
-    planet.$planet.click(function (e) {
-        var x = e.pageX;
-        var y = e.pageY;
-        alert(collides(x, y));
-    });
+    var planet = {
+        $planet: $elem,
+        radiusField: $gravity.width() / 2,
+        radiusPlanet: $elem.find('.planet-surface').width() / 2,
+        centerX: $gravity.offset().left + $gravity.width() / 2,
+        centerY: $gravity.offset().top + $gravity.height() / 2,
+        collides: collides,
+        overlaps: overlapsField
+    };
     
-    planets.push(planet);
+    return planet;
+}
+
+function init($elem) {
+    planets.push(createPlanet($elem));
 }
 
 function allPlanets() {
@@ -38,3 +70,4 @@ function allPlanets() {
 
 exports.init = init;
 exports.allPlanets = allPlanets;
+exports.randomPlanet = randomPlanetDiv;
